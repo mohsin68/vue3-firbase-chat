0aaab2c0206e05b77df9a35367b117c3c2051aae
Ahmed Mohsin <mohsendev68@gmail.com>
2023-12-02 16:35:24 +0200

    Update sign-in UI in Auth.vue

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -2,14 +2,14 @@
   <div
     class="auth h-full text-center flex items-center justify-center flex-col"
   >
-    <h1 class="">Welcome to the vue firebase chat app</h1>
+    <h1 class="font-sans">Sign In To Start Chating</h1>
     <button
       class="bg-primary text-dark rounded px-8 py-2 mt-4 text-lg"
       @click="signIn"
     >
       <img
-        class="inline-block w-6 h-6 mr-2"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1176px-Google_%22G%22_Logo.svg.png?20230305195327"
+        class="inline-block w-8 h-8 mr-2"
+        src="@/assets/images/google.png"
         alt="Google"
       />
       Sign in with Google
